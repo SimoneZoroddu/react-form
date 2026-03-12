3a747be137a1af74b55c549b318ad20d79058a17
SimoneZoroddu <lenviur@gmail.com>
2026-03-12 14:19:58 +0100

    adding usestate for take the value from the input and use

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react"
 
 
 
-export default function AppMain(){
+export default function AppMain() {
 
+    const [value, setValue] = useState('')
 
+    function changeValue(e) {
+        e.preventDefault()
+
+    }
 
     return (
         <>
             <article>
-                
+                <h1>Titolo</h1>
+            </article>
+            <article>
+                <h1>Titolo</h1>
+            </article>
+            <article>
+                <h1>Titolo</h1>
+            </article>
+            <article>
+                <h1>Titolo</h1>
             </article>
 
+            <form action="" onSubmit={changeValue}>
+                <input type="text" placeholder="Titolo da inserire nell articolo" value={value} onChange={(e) => setValue(e.target.value)}/>
+            </form>
         </>
     )
 }
