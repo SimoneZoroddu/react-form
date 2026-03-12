111a10ba67b8a9375e918bb982e76f672a0fc4c4
SimoneZoroddu <lenviur@gmail.com>
2026-03-12 14:41:45 +0100

    end milestone 2

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -1,21 +1,34 @@
-import { useState } from "react"
+import { use, useState } from "react"
 
 
 
 export default function AppMain() {
+    const arrayArticles = ['']
 
     const [value, setValue] = useState('')
+    const [valueSaved, setValueSaved] = useState(arrayArticles)
+
+
+
 
     function changeValue(e) {
         e.preventDefault()
-
+        setValueSaved([value, ...valueSaved])
     }
 
     return (
         <>
-            <article>
-                <h1>Titolo</h1>
-            </article>
+            <form action="" onSubmit={changeValue}>
+                <input type="text" placeholder="Titolo da inserire nell articolo" value={value} onChange={(e) => setValue(e.target.value)} />
+            </form>
+            {
+                valueSaved.map((article, index) => (
+                    <article key={index}>
+                        <h1>{article}</h1>
+                    </article>
+
+                ))
+            }
             <article>
                 <h1>Titolo</h1>
             </article>
@@ -26,9 +39,6 @@ export default function AppMain() {
                 <h1>Titolo</h1>
             </article>
 
-            <form action="" onSubmit={changeValue}>
-                <input type="text" placeholder="Titolo da inserire nell articolo" value={value} onChange={(e) => setValue(e.target.value)}/>
-            </form>
         </>
     )
 }
