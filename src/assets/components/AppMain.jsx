6636b5f92fce0cf68d94a3f870de5d289899bff9
SimoneZoroddu <lenviur@gmail.com>
2026-03-15 11:54:25 +0100

    try to add the change... work but not in the right place with article.index

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -3,42 +3,79 @@ import { use, useState } from "react"
 
 
 export default function AppMain() {
-    const arrayArticles = ['']
+
+
 
     const [value, setValue] = useState('')
-    const [valueSaved, setValueSaved] = useState(arrayArticles)
-
-
+    const [valueSaved, setValueSaved] = useState([])
 
 
     function changeValue(e) {
         e.preventDefault()
         setValueSaved([value, ...valueSaved])
+        setValue('')
     }
+
+    function filteredArray(indexMap) {
+        const newArrayFiltered = valueSaved.filter((singleValue, index) => index !== indexMap)
+        setValueSaved(newArrayFiltered);
+
+    }
+
+
+
+
+
+    
+    const [boolean, setBoolean] = useState(false)
+
+
+    function changeName() {
+        if (boolean == true) {
+            setBoolean(false)
+        } else {
+
+            setBoolean(true)
+        }
+    }
+
+
+    const [newValue, setnewValue] = useState('')
+
+
 
     return (
         <>
-            <form action="" onSubmit={changeValue}>
-                <input type="text" placeholder="Titolo da inserire nell articolo" value={value} onChange={(e) => setValue(e.target.value)} />
-            </form>
-            {
-                valueSaved.map((article, index) => (
-                    <article key={index}>
-                        <h1>{article}</h1>
-                    </article>
+            <div className="container d-flex justify-content-center my-5">
+                <form action="" onSubmit={changeValue} className="text-center">
+                    <input className="input-group-text" type="text" placeholder="Titolo per l'articolo" value={value} onChange={(e) => setValue(e.target.value)} />
+                    <button className="btn btn-secondary mt-2">Add</button>
+                </form>
+            </div>
+            <div className="container text-center">
+                {
+                    valueSaved.length >= 1 && valueSaved.map((article, index) => (
+                        <article key={index} className="d-flex justify-content-center" >
+                            {boolean
+                                ?
+                                <input className="input-group-text" type="text" placeholder={article} value={newValue} onChange={(e) => setnewValue(e.target.value)} />
+                                :
+                                <>
+                                    {   newValue
+                                        ?
+                                        <h1>{newValue}</h1>
+                                        :
+                                        <h1>{article}</h1>
+                                    }
+                                </>
+                            }
+                            <button className="btn btn-danger m-2" onClick={() => filteredArray(index)}>🗑️</button>
+                            <button className="btn btn-primary m-2" onClick={() => changeName(index)}>📝</button>
+                        </article>
 
-                ))
-            }
-            <article>
-                <h1>Titolo</h1>
-            </article>
-            <article>
-                <h1>Titolo</h1>
-            </article>
-            <article>
-                <h1>Titolo</h1>
-            </article>
-
+                    ))
+                }
+            </div>
         </>
     )
 }
