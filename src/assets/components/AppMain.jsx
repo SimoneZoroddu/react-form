8d0ebad27f0ab8ab56e678e8754ef6c9496b9e42
SimoneZoroddu <lenviur@gmail.com>
2026-03-23 20:08:08 +0100

    add bonus with change

--- src/assets/components/AppMain.jsx
+++ src/assets/components/AppMain.jsx
@@ -9,6 +9,7 @@ export default function AppMain() {
     const [value, setValue] = useState('')
     const [valueSaved, setValueSaved] = useState([])
 
+    const [current, setCurrent] = useState(-1)
 
     function changeValue(e) {
         e.preventDefault()
@@ -22,30 +23,29 @@ export default function AppMain() {
 
     }
 
-
-
-
-
-    
-    const [boolean, setBoolean] = useState(false)
-
-
-    function changeName() {
-        if (boolean == true) {
-            setBoolean(false)
-        } else {
-
-            setBoolean(true)
-        }
-    }
-
-
+    ///////////////////////////////////////////////////////////
     const [newValue, setnewValue] = useState('')
+
+
+
+    function changeFilteredArray(index) {
+        const newValueSaved = valueSaved.map((article, i) => {
+            if (i === index) {
+                return newValue
+            }
+            return article
+        })
+
+        setValueSaved(newValueSaved)
+        setCurrent(-1)
+        setnewValue('')
+    }
 
 
 
     return (
         <>
+            <div>{current}</div>
             <div className="container d-flex justify-content-center my-5">
                 <form action="" onSubmit={changeValue} className="text-center">
                     <input className="input-group-text" type="text" placeholder="Titolo per l'articolo" value={value} onChange={(e) => setValue(e.target.value)} />
@@ -56,21 +56,21 @@ export default function AppMain() {
                 {
                     valueSaved.length >= 1 && valueSaved.map((article, index) => (
                         <article key={index} className="d-flex justify-content-center" >
-                            {boolean
-                                ?
-                                <input className="input-group-text" type="text" placeholder={article} value={newValue} onChange={(e) => setnewValue(e.target.value)} />
-                                :
-                                <>
-                                    {   newValue
-                                        ?
-                                        <h1>{newValue}</h1>
-                                        :
+                            {
+                                current == index
+                                    ?
+                                    <>
+                                        <input className="input-group-text" type="text" placeholder={article} value={newValue} onChange={(e) => setnewValue(e.target.value)} />
+                                        <button className="btn btn-primary m-2" onClick={() => changeFilteredArray(index)}>✍️</button>
+                                    </>
+                                    :
+                                    <>
                                         <h1>{article}</h1>
-                                    }
-                                </>
+                                        <button className="btn btn-danger m-2" onClick={() => filteredArray(index)}>🗑️</button>
+                                        <button className="btn btn-primary m-2" onClick={() => setCurrent(index)}>📝</button>
+                                    </>
+
                             }
-                            <button className="btn btn-danger m-2" onClick={() => filteredArray(index)}>🗑️</button>
-                            <button className="btn btn-primary m-2" onClick={() => changeName(index)}>📝</button>
                         </article>
 
                     ))
@@ -79,3 +79,51 @@ export default function AppMain() {
         </>
     )
 }
+
+
+
+
+/* {
+    boolean
+    ?
+    :
+    <>
+        {   newValue
+            ?
+            <h1>{newValue}</h1>
+            :
+        }
+    </>
+} */
+
+
+
+
+/* 
+const [boolean, setBoolean] = useState(false)
+
+
+function changeName(indexMap) {
+
+
+
+valueSaved.forEach((singleValue, index) => if (index !== indexMap) {
+setBoolean(true) 
+else {
+    setBoolean(false)}
+    }
+    )
+
+
+.filter((singleValue, index) => {
+    if (indexMap = index) {
+        setBoolean(false)
+    } else {
+
+        setBoolean(true)
+    }
+})
+}
+
+
+*/
